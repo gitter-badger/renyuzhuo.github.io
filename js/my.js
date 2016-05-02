88e1e7402f8267b59962e065553cbe271e31a6d4
renyuzhuo <renyuzhuo@foxmail.com>
2016-05-03 01:44:32 +0800

    Site updated: 2016-05-03 01:44:31

---
+++ js/my.js
@@ -0,0 +1,20 @@
+/**
+ * google蜘蛛
+ */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-72013003-1', 'auto');
+ga('send', 'pageview');
+
+/**
+ * 百度自动推送
+ */
+(function(){
+    var bp = document.createElement('script');
+    bp.src = '//push.zhanzhang.baidu.com/push.js';
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
